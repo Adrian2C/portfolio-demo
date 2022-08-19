c8c394713e29d9ab82280a37cece06506d36c8ad
Adrian Ezequiel COCERES <acoceres@edenor.com>
2022-08-19 11:53:03 -0300

    add js mixitup, card, popup

--- js/script.js
+++ js/script.js
@@ -25,9 +25,39 @@ const tabs = document.querySelectorAll('[data-target]'),
     })
 })
 /*===============MIXITUP FILTER ORTFOLIO================= */
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 /*===============LINK ACTIVE WORK================= */
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(L=> I.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkWork.forEach(L=> l.addEventListener("click",activeWork))
 /*===============LINK ACTIVE WORK================= */
 /*===============WORK POPUP================= */
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+}
 /*===============SERVICES MODAL================= */
 /*===============SWIPER TESTIMONIAL================= */
 /*===============SIDEBARSHOW================= */

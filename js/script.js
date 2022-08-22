0b845e2041322463c52fa86021924435635a8006
Adrian2C <acoceres@edenor.com>
2022-08-22 14:57:07 -0300

    fin de arreglos en Pop Up, inicio de servicios

--- js/script.js
+++ js/script.js
@@ -45,21 +45,21 @@ linkWork.forEach(l=> l.addEventListener("click", activeWork))
 /*===============LINK ACTIVE WORK================= */
 /*===============WORK POPUP================= */
 document.addEventListener("click", (e) => {
-    if(e.target.classList.contains("work__button")){
-        togglePortfolioPopup();
-        portfolioItemDetails(e.target.parentElement);
+    if (e.target.classList.contains("work__button")) {
+      togglePortfolioPopup();
+      portfolioItemDetails(e.target.parentElement);
     }
 })
 
 function togglePortfolioPopup() {
     document.querySelector(".portfolio__popup").classList.toggle("open");
 }
-document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup)
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
 
 function portfolioItemDetails(portfolioItem){
     document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
-    document.querySelector(".portfolio2__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
-    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector("portfolio__item-details").innerHTML;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
 /*===============SERVICES MODAL================= */
 /*===============SWIPER TESTIMONIAL================= */
